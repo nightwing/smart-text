ba5bd63b5119c0a6bd80c5b17daf0cf291c47ccf
h.a <amirjanyan@gmail.com>
2011-03-22 16:33:20 +0400

    restartless version

--- bootstrap.js
+++ bootstrap.js
@@ -60,22 +60,7 @@ WindowListener={
  * bootstrap.js API
  *****************/
 function startup(aData, aReason) {
-	Components.utils.import("resource://gre/modules/Services.jsm");
-	let resource = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
-	let alias = Services.io.newFileURI(aData.installPath);
-	if (!aData.installPath.isDirectory())
-		alias = Services.io.newURI("jar:" + alias.spec + "!/", null, null);
-	resource.setSubstitution("myaddonpackage", alias);
-	
-	
-	var io = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService)
-	var resource = io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
-	var alias = io.newURI(__SCRIPT_URI_SPEC__+'/../content/', null, null)
-	resource.setSubstitution("smarttext", alias);
-	
-	/*var alias = Services.io.newFileURI(aData.installPath);
-	if (!aData.installPath.isDirectory())
-		alias = Services.io.newURI("jar:" + alias.spec + "!/", null, null);*/
+	AddJarManifestLocation(aData.installPath.path)
 	
 	var file=getCssFile()
 	if(file.exists())
@@ -85,11 +70,19 @@ function startup(aData, aReason) {
 }
 
 function shutdown(aData, aReason) {
-	var io = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService)
-	var resource = io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
-	resource.setSubstitution("smarttext", null);
+	if (aReason == APP_SHUTDOWN)
+		return;
+		
+	let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);	
+	// close option windows if any
+	let enumerator = wm.getEnumerator("");
+	while(enumerator.hasMoreElements()) {
+		let win = enumerator.getNext();
+		if(win.location.href.indexOf('chrome://smarttext/content/')==0)
+			win.close()
+	}
 	
-	let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
 	wm.removeListener(WindowListener);
 
 	updateStyle(false, getCssFile())	
@@ -102,87 +95,61 @@ function uninstall(aData, aReason){
 }
 
 
+function AddJarManifestLocation(path) {
+	Components.utils.import("resource://gre/modules/ctypes.jsm");
+	var file = Cc["@mozilla.org/file/directory_service;1"]
+				.getService(Ci.nsIProperties)
+				.get("resource:app", Ci.nsIFile);
+	file.append(ctypes.libraryName("xul"));
+	var libxul = ctypes.open(file.path);
+  
+	// we need to explicitly allocate a type for the buffer we'll need to hold
+	// the path in :(
+	var bufLen = path.length + 2;
+	var PathBuffer_t = ctypes.StructType("PathBuffer",
+										[{buf: ctypes.jschar.array(bufLen)}])
+	var nsString_t = ctypes.StructType("nsAString",
+										[{mData:   PathBuffer_t.ptr}
+										,{mLength: ctypes.uint32_t}
+										,{mFlags:  ctypes.uint32_t}])
+	var PRBool_t = ctypes.uint32_t; // yay NSPR
+	var nsILocalFile_t = ctypes.StructType("nsILocalFile").ptr;
 
-
-/*************************************************************************************
- *
- *
- ******************************************//*
- const Cc = Components.classes;
-const Ci = Components.interfaces;
-
-var WindowListener = {
-	setupBrowserUI: function(window) {
-		let document = window.document;
-
-		// Take any steps to add UI or anything to the browser window
-		// document.getElementById() etc. will work here
-	},
-
-	tearDownBrowserUI: function(window) {
-		let document = window.document;
-
-		// Take any steps to remove UI or anything from the browser window
-		// document.getElementById() etc. will work here
-	},
-
-	// nsIWindowMediatorListener functions
-	onOpenWindow: function(xulWindow) {
-		// A new window has opened
-		let domWindow = xulWindow.QueryInterface(Ci.nsIInterfaceRequestor)
-								.getInterface(Ci.nsIDOMWindowInternal);
-
-		// Wait for it to finish loading
-		domWindow.addEventListener("load", function listener() {
-			domWindow.removeEventListener("load", listener, false);
-
-			// If this is a browser window then setup its UI
-			if (domWindow.document.documentElement.getAttribute("windowtype") == "navigator:browser")
-				WindowListener.setupBrowserUI(domWindow);
-		}, false);
-	},
-
-	onCloseWindow: function(xulWindow) {
-	},
-
-	onWindowTitleChange: function(xulWindow, newTitle) {
+	var NS_NewLocalFile = libxul.declare("NS_NewLocalFile_P",
+                   ctypes.default_abi,
+                   ctypes.uint32_t,         // nsresult return
+                   nsString_t.ptr,          // const nsAString &path
+                   PRBool_t,                // PRBool followLinks
+                   nsILocalFile_t.ptr       // nsILocalFile* *result
+	);
+	var XRE_AddJarManifestLocation = libxul.declare("XRE_AddJarManifestLocation",
+                   ctypes.default_abi,
+                   ctypes.uint32_t,         // nsresult return
+                   ctypes.int32_t,          // NSLocationType aType
+                   nsILocalFile_t           // nsILocalFile* aLocation
+	);
+	var pathBuffer = new PathBuffer_t;
+	pathBuffer.buf = path + '\0';
+	var manifest = new nsString_t;
+	manifest.mData = pathBuffer.address();
+	manifest.mLength = path.length;
+	manifest.mFlags = 1 << 4; // F_FIXED
+	var manifestPtr = manifest.address();
+  
+	try {
+		var rv;
+		var localFile = new nsILocalFile_t;
+		rv = NS_NewLocalFile(manifest.address(), false, localFile.address());
+		if (rv & 0x80000000) {
+			throw Components.Exception("NS_NewLocalFile error", rv);
+		}
+		const NS_SKIN_LOCATION = 1;
+		rv = XRE_AddJarManifestLocation(NS_SKIN_LOCATION, localFile);
+		if (rv & 0x80000000) {
+			throw Components.Exception("XRE_AddJarManifestLocation error", rv);
+		}
+	} finally {
+		libxul.close();
 	}
-};
-
-function startup(data, reason) {
-	let wm = Cc["@mozilla.org/appshell/window-mediator;1"].
-           getService(Ci.nsIWindowMediator);
-
-	// Get the list of browser windows already open
-	let windows = wm.getEnumerator("navigator:browser");
-	while (windows.hasMoreElements()) {
-		let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-
-		WindowListener.setupBrowserUI(domWindow);
-	}
-
-	// Wait for any new browser windows to open
-	wm.addListener(WindowListener);
 }
 
-function shutdown(data, reason) {
-	// When the application is shutting down we normally don't have to clean
-	// up any UI changes made
-	if (reason == APP_SHUTDOWN)
-		return;
-
-	let wm = Cc["@mozilla.org/appshell/window-mediator;1"].
-           getService(Ci.nsIWindowMediator);
-
-	// Get the list of browser windows already open
-	let windows = wm.getEnumerator("navigator:browser");
-	while (windows.hasMoreElements()) {
-		let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-
-		WindowListener.tearDownBrowserUI(domWindow);
-	}
-
-	// Stop listening for any new browser windows to open
-	wm.removeListener(WindowListener);
-}
-*/
